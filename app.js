37de184d6ce47d0f23c51032c83d6389b5b62224
Phuong  Anh <phuonganhphys1905@gmail.com>
2022-11-24 12:30:39 +0700

    feat: implement calculating with previous value

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-let contentData = "";
+let contentData = "0";
 let resultData = "";
+let previousData = "";
+
 function renderKeyBroad() {
   const KEYS = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", ".", "C"];
   const numberKeyElement = document.getElementById("number-key");
@@ -16,11 +18,16 @@ function onClickKeyBoard(event) {
   const currentKeyElement = event.target;
   const keyContent = currentKeyElement.textContent;
   if (keyContent === "C") {
-    contentData = "";
-    resultData = "";
+    resetData();
     renderCalculator();
     renderResult();
   } else {
+    if (checkTheFirstSave() || contentData === "0") {
+      contentData = "";
+      resultData = "";
+      previousData = "";
+    }
+
     contentData += keyContent;
     renderCalculator();
   }
@@ -33,6 +40,10 @@ function handleCalculatorKey() {
     item.addEventListener("click", (event) => {
       const currentItemElement = event.target;
       const childKey = currentItemElement.dataset.key;
+      if (previousData) {
+        contentData = "";
+      }
+
       contentData += childKey;
       renderCalculator();
     });
@@ -43,8 +54,9 @@ function handleEqualKey() {
   const equalKeyElement = document.getElementById("equal-key");
   equalKeyElement.addEventListener("click", (event) => {
     try {
-      const result = eval(contentData);
+      const result = eval(previousData + contentData);
       resultData = result;
+      previousData = result;
       renderResult();
     } catch (error) {
       renderResult(true);
@@ -54,12 +66,59 @@ function handleEqualKey() {
 
 function renderCalculator() {
   const calculatorElement = document.getElementById("calculator-content");
-  calculatorElement.innerHTML = contentData;
+
+  const formattedContentData = formatContentData(contentData);
+
+  calculatorElement.innerHTML =
+    `${previousData ? "Ans" : ""}` + formattedContentData;
 }
 
 function renderResult(isError) {
   const resultElement = document.getElementById("result-content");
   resultElement.innerHTML = isError ? "Error" : resultData;
+}
+
+function resetData() {
+  resultData = "";
+  contentData = "0";
+  previousData = "";
+}
+
+function formatContentData(currentContentData) {
+  let newContentData = `${currentContentData}`;
+
+  for (let index = 0; index < newContentData.length; index++) {
+    const currentChar = newContentData[index];
+    switch (currentChar) {
+      case "*":
+        newContentData = replaceAt(newContentData, index, "x");
+        break;
+      case "/":
+        newContentData = replaceAt(newContentData, index, ":");
+        break;
+      default:
+        break;
+    }
+  }
+
+  return newContentData;
+}
+
+function replaceAt(value, index, replacement) {
+  return (
+    value.substring(0, index) +
+    replacement +
+    value.substring(index + replacement.length)
+  );
+}
+
+function checkTheFirstSave() {
+  const calculatorElement = document.getElementById("calculator-content");
+  const calculatorContent = calculatorElement.textContent;
+  if (!calculatorContent.includes("Ans") && previousData) {
+    return true;
+  }
+  return false;
 }
 
 function main() {
